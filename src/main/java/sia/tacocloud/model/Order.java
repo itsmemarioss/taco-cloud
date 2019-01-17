2058dc6862a6d532dba6d1c5b61d8b1be55dc429
Mario <super.mario.santos.sousa@gmail.com>
2019-01-17 17:14:06 -0300

    add orderController

---
+++ src/main/java/sia/tacocloud/model/Order.java
@@ -0,0 +1,8 @@
+package sia.tacocloud.model;
+
+import lombok.Data;
+
+@Data
+public class Order {
+
+}
--- src/main/java/sia/tacocloud/model/Taco.java
+++ src/main/java/sia/tacocloud/model/Taco.java
@@ -1,9 +1,13 @@
 package sia.tacocloud.model;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class Taco {
 
 	private String name;
+	private List<String> ingredients;
+
 }
