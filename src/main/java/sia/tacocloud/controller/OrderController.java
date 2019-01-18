6d609b7da4032b3c82b1ae4878d9fa88ac3bfd17
Mario <super.mario.santos.sousa@gmail.com>
2019-01-18 17:29:43 -0300

    finishing cap 2

--- src/main/java/sia/tacocloud/controller/OrderController.java
+++ src/main/java/sia/tacocloud/controller/OrderController.java
@@ -1,8 +1,12 @@
 package sia.tacocloud.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +21,15 @@ public class OrderController {
 	public String orderForm(Model model) {
 		model.addAttribute("order", new Order());
 		return "orderForm";
+	}
+
+	@PostMapping
+	public String processOrder(@Valid Order order, Errors erros) {
+		if(erros.hasErrors())
+			return "orderForm";
+		
+		log.info("Order submitted: " + order);
+		return "redirect:/";
 	}
 
 }
